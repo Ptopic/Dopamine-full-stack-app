ab5de8a12bd574ed1fb0b83436bb7dcec2bf547a
unknown <pingo15102002@gmail.com>
2023-04-01 01:14:40 +0200

    Backend - otp verification route completed

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -21,7 +21,7 @@ exports.checkUserName = async (req, res, next) => {
 			console.log(result.length);
 			return res
 				.status(400)
-				.json({ success: false, error: 'Pick a different username' });
+				.send({ success: false, error: 'Pick a different username' });
 		}
 	});
 };
@@ -32,12 +32,12 @@ exports.createUser = async (req, res, next) => {
 	const hash = await bcrypt.hash(password, 8);
 	const uid = uuidv4();
 	const sqlInsert = `INSERT INTO users (uid, name, email, password) VALUES ('${uid}', '${name}', '${email}', '${hash}')`;
-
+	console.log(uid);
 	// Check if user with same name exists if it does return error
 
 	db.query(sqlInsert, (err, result) => {
 		if (err) {
-			return res.status(400).json({
+			return res.status(400).send({
 				success: false,
 				error: 'Username or email need to be different',
 			});
@@ -74,10 +74,8 @@ exports.createAuthToken = async (req, res) => {
 	};
 	mailTransport().sendMail(mailOptions, function (err, info) {
 		if (err) {
-			console.log(err);
+			return res.status(400).send({ success: false, error: 'Cant send email' });
 		} else {
-			console.log(info);
-			res.send('Success');
 		}
 	});
 	const sqlInsert = `INSERT INTO authTokens
@@ -86,9 +84,9 @@ exports.createAuthToken = async (req, res) => {
 
 	db.query(sqlInsert, (err, result) => {
 		if (err) {
-			return res.status(400).json({ success: false, error: err.code });
+			return res.status(400).send({ success: false, error: err.code });
 		}
-		return res.status(200).json({ success: true, error: 'Success' });
+		return res.status(200).send({ success: true, error: 'Success' });
 	});
 
 	// Create cron job to delete auth record after 2 minutes
@@ -119,24 +117,24 @@ exports.signIn = async (req, res) => {
 	if (!email.trim() || !password.trim())
 		return res
 			.status(400)
-			.json({ success: false, error: 'Email/password missing!' });
+			.send({ success: false, error: 'Email/password missing!' });
 	// Find user with email
 	const userReturned = await getUserFromEmail(req.body.email);
 	const user = userReturned[0];
 	// If user not found send error
 	if (!user)
-		return res.status(400).json({ success: false, error: 'User not found!' });
+		return res.status(400).send({ success: false, error: 'User not found!' });
 
 	const isMatch = await signInUser(req.body.password, user.password);
 	if (!isMatch)
 		return res
 			.status(400)
-			.json({ success: false, error: 'Email/password not matching!' });
+			.send({ success: false, error: 'Email/password not matching!' });
 
 	const token = jwt.sign({ userId: user.uid }, process.env.JWT_SECRET, {
 		expiresIn: '1d',
 	});
-	res.json({
+	res.send({
 		success: true,
 		user: {
 			user: user.name,
@@ -151,35 +149,98 @@ exports.verifyUser = async (req, res) => {
 	const { userId, otp } = req.body;
 
 	if (!userId || !otp.trim())
-		return res.status(400).json({
+		return res.status(400).send({
 			success: false,
 			error: 'Invalid request, missing otp or userId',
 		});
 
 	// Find user and
-	const findQuery = `SELECT * FROM users WHERE uid = "${userId}"; SELECT * FROM authTokens WHERE ownerId = "${userId}"`;
-
-	db.query(findQuery, (err, result) => {
+	const findQuery = `SELECT * FROM users WHERE uid = "${userId}"`;
+	const findQueryTokens = `SELECT * FROM authTokens WHERE ownerId = "${userId}"`;
+	db.query(findQuery, async (err, result) => {
 		if (err) {
-			return res.status(400).json({ success: false, error: err.code });
+			return res.status(400).send({ success: false, error: err.code });
 		}
 
-		if (result[0].isVerified)
+		if (result[0] == undefined) {
 			return res
 				.status(400)
-				.json({ success: false, error: 'This user is already verified' });
+				.send({ success: false, error: 'user or token not found' });
+		}
 
-		if (result[1].length < 1)
-			return res
-				.status(400)
-				.json({ success: false, error: 'Sorry, user not found' });
+		res.locals.user = result[0];
+		// console.log(result[0]);
 
-		// Compare given otp to user otp
+		db.query(findQueryTokens, async (err, result) => {
+			if (err) {
+				return res.status(400).send({ success: false, error: err.code });
+			}
 
-		if (!result[1].token == otp)
-			return res
-				.status(400)
-				.json({ success: false, error: 'Please provide a valid token' });
-		return res.send(result);
+			if (result[0] == undefined) {
+				return res
+					.status(400)
+					.send({ success: false, error: 'user or token not found' });
+			}
+			res.locals.token = result[0];
+			// console.log(result[0]);
+
+			// // console.log(result[0][0].email);
+			// res.locals.email = result[0][0].email;
+			if (res.locals.user.isVerified)
+				return res
+					.status(400)
+					.send({ success: false, error: 'This user is already verified' });
+
+			// Compare given otp to user otp
+			const compareTokens = bcrypt.compareSync(otp, res.locals.token.token);
+
+			if (!compareTokens)
+				return res
+					.status(400)
+					.send({ success: false, error: 'Please provide a valid token' });
+
+			// set user isVerified to true
+			const verifyChangeQuery = `UPDATE users SET isVerified = 1 WHERE uid = "${userId}"`;
+
+			db.query(verifyChangeQuery, (err, result) => {
+				if (err) {
+					return res.status(400).send({
+						success: false,
+						error: 'User verification status cant be changed',
+					});
+				}
+
+				// Delete token row from authTokens db
+
+				const deleteAuthTokenQuery = `DELETE FROM authTokens WHERE ownerId = "${userId}"`;
+
+				db.query(deleteAuthTokenQuery, (err, result) => {
+					if (err) {
+						return res.status(400).send({
+							success: false,
+							error: 'Unable to delete auth token from db',
+						});
+					}
+				});
+
+				// Get user email to send verified notifcation to
+				console.log(res.locals.user.email);
+				// Send email verified email
+				const mailOptions = {
+					from: 'email@email.com',
+					to: res.locals.user.email,
+					subject: 'Email verified',
+					html: '<h1>Verified</h1>',
+				};
+				mailTransport().sendMail(mailOptions, function (err, info) {
+					if (err) {
+						console.log(err);
+					} else {
+						console.log(info);
+						res.send('Success');
+					}
+				});
+			});
+		});
 	});
 };
