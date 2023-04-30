dfd6882621747774b40c205f6e797a5a979400b3
unknown <pingo15102002@gmail.com>
2023-05-01 00:16:51 +0200

    Changes

--- backend/controllers/codes.js
+++ backend/controllers/codes.js
@@ -1,36 +1,42 @@
 const CountryCodes = require('../utils/CountryCodes.json');
 
 exports.checkCode = async (req, res, next) => {
-	// Find by country code (HR) or country by phone code (+385)
-	const { code } = req.query;
+	try {
+		// Find by country code (HR) or country by phone code (+385)
+		const { code } = req.query;
 
-	// Regex to check if code is country code or phone code
-	// Check for +385 regex -> /\+[0-9]+/g
-	// Check only for numbers regex -> /.[0-9]+/g
-	const isPhoneCode = code.match(/.[0-9]+/g);
+		console.log(code);
 
-	if (isPhoneCode) {
-		const phoneString = '+' + code;
+		// Regex to check if code is country code or phone code
+		// Check for +385 regex -> /\+[0-9]+/g
+		// Check only for numbers regex -> /.[0-9]+/g
+		const isPhoneCode = code.match(/.[0-9]+/g);
 
-		const found = CountryCodes.find((el) => el.dial_code == phoneString);
+		if (isPhoneCode) {
+			const phoneString = '+' + code;
 
-		if (!found) {
-			res.send({ success: false });
+			const found = CountryCodes.find((el) => el.dial_code == phoneString);
+
+			if (!found) {
+				res.send({ success: false });
+			} else {
+				res.data = found;
+				next();
+			}
 		} else {
-			res.data = found;
-			next();
-		}
-	} else {
-		const codeUpper = code.toUpperCase();
-		console.log(codeUpper);
+			const codeUpper = code.toUpperCase();
+			console.log(codeUpper);
 
-		const found = CountryCodes.find((el) => el.code == codeUpper);
-		console.log(found);
-		if (!found) {
-			res.send({ success: false });
-		} else {
-			res.data = found;
-			next();
+			const found = CountryCodes.find((el) => el.code == codeUpper);
+			console.log(found);
+			if (!found) {
+				res.send({ success: false });
+			} else {
+				res.data = found;
+				next();
+			}
 		}
+	} catch (error) {
+		console.log(error);
 	}
 };
